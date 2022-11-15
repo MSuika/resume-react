4cf79d6edcd16b7aeca1e30a77e1ad7d52d5dc49
MSuika <markassuika@gmail.com>
2022-11-15 18:18:15 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       https://prnt.sc/A0eYVEjRYScJ
+      <p>testing</p>
     </div>
   );
 }
